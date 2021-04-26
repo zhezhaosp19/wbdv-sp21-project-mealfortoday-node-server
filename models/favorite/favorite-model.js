955af89cbe4a20f31d2b5d036f40829a7c0baceb
Namuhan Muschin <namuhan@Namuhans-MacBook-Pro.local>
2021-04-26 00:17:19 -0700

    Corrected typo

--- models/favorite/favorite-model.js
+++ models/favorite/favorite-model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const favoritesSchema = require('./favorites-schema')
 
 const favoritesModel = mongoose.model(
-    "FavoritesModel",
+    "favoritesModel",
     favoritesSchema
 )
 
